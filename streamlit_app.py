a9e20f22e95bd5931488d797c921857fec06c06a
ZaxOne <muhammadzaki010307@gmail.com>
2026-05-22 10:29:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 JURNALIKA")
+st.title("🎈 Jurnalika")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Kami Bnagga Jadi PERS Kampus [jurnalika.com](https://www.jurnalikanews.com/)."
 )
